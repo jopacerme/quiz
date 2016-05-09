40f4a05baefa935a6944bb31c5288c3b4f303a0f
jopacerme <jopacerm@gmail.com>
2016-05-09 22:56:19 +0200

    Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,14 @@ router.get('/', function(req, res, next) {
 router.param('quizId', quizController.load);
 
 //router.get('/question', quizController.question);
-router.get('/quizzes', quizController.index);
-router.get('/quizzes/:quizId(\\d+)' , quizController.show)
+router.get('/quizzes.:format?', quizController.index);
+router.get('/quizzes/:quizId(\\d+).:format?' , quizController.show)
 router.get('/quizzes/:quizId(\\d+)/check',	quizController.check);
 router.get('/credits', quizController.credits);
 router.get('/quizzes/new', quizController.new);
 router.post('/quizzes', quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizzes/:quizId(\\d+)', quizController.update);
+router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
 
 module.exports = router;
